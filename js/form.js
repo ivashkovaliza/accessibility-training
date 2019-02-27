bf450d2d42a251e3dda41a0aa4a8cb84ada82559
ivashkovaliza <ivashkovaliza@gmail.com>
2019-02-28 00:54:38 +0300

    add error messages

--- js/form.js
+++ js/form.js
@@ -1,10 +1,34 @@
 
 document.querySelector('#submit-btn').addEventListener('click', addDataFromForm);
 var teamMemmbersArray = [];
+var errorsAmount = 0;
+var errorsArray = [];
+var $regionnotice = $('<div class="regionnotice help is-danger" tabindex="-1">'),
+$errorHeading = $('<h3>Error! The form could not be submitted due to invalid entries.</h3><p>Please fix the following fields:</p>'),
+$listOfErrors = $('<ol>'),
+$usernameError = $('<p id="username-error" class="elementnotice help is-danger">This username is unavailable, please enter new username</p>');
 
-function addDataFromForm() {
-    checkValidityUsername();
-    checkValidityEmail();
+function addDataFromForm(e) {
+    e.preventDefault();
+    resetFormState();
+    checkValidityUsername();   
+    if(errorsAmount) {
+        $regionnotice.append($errorHeading);
+        $regionnotice.append($listOfErrors);
+        errorsArray.forEach( element => {
+            $listOfErrors.append(element)
+        });
+        $('#team-registration-form').prepend($regionnotice);
+        $regionnotice.focus();
+        return;
+    } 
+    //checkValidityEmail();
+    //checkValidityPhone();
+    //checkEqualNameAndSurname();
+    //if(!document.getElementById('year-of-birth').hasAttribute('disabled')) {
+      //  checkValidityYear();
+    //}    
+
     teamMemmbersArray.push({
         username: document.getElementById('username').value,
         //name: document.getElementById('name').value,
@@ -13,10 +37,10 @@ function addDataFromForm() {
         //address: document.getElementById('address').value,
         //yearOfBirth: document.getElementById('year-of-birth').value,
         //postcode: document.getElementById('postcode').value,
-        email: document.getElementById('email').value
+        //email: document.getElementById('email').value
     });
     console.log(teamMemmbersArray);
-    resetFormValues();
+    //resetFormValues();
     alert('The form is successfully submitted');
 }
 
@@ -25,9 +49,14 @@ function resetFormValues() {
 }
 
 function checkValidityUsername() {
+    $usernameError.remove();
     teamMemmbersArray.forEach(element => {
-        if(element.username === document.getElementById('username').value) {
-            console.log('username invalid');
+        if(element.username === document.getElementById('username').value || document.getElementById('username').value === "") {
+            errorsAmount++;
+            $('#username-field').append($usernameError);
+            errorsArray.push($('<li><a href="#username">This username is unavailable, please enter new username</a></li>'));
+        } else {
+            document.getElementById('username').setAttribute('aria-invalid','false');
         }
     });    
 }
@@ -37,4 +66,30 @@ function checkValidityEmail() {
     if(!re.test(document.getElementById('email').value)) {
         console.log('email invalid');
     }
+}
+
+function checkValidityPhone() {   
+    if(!/^[0-9]+$/.test(document.getElementById('phone-number').value)) {
+        console.log('phone invalid');
+    }        
+}
+
+function checkEqualNameAndSurname() {
+    teamMemmbersArray.forEach(element => {
+        if(element.name === document.getElementById('name').value && element.surname === document.getElementById('surname').value) {
+            document.getElementById('year-of-birth').removeAttribute('disabled');
+            document.getElementById('year-of-birth').setAttribute('aria-disabled','true');
+        }
+    });
+}
+
+function checkValidityYear() {
+    if(!/^[1-9][0-9]{3}$/.test(document.getElementById('year-of-birth').value)) {
+        console.log('year of birth invalid');
+    }
+}
+
+function resetFormState() {
+    $regionnotice.remove();
+    errorsAmount = 0;
 }
