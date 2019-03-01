3f7f9f92437561658cefc1e5f444f4a603f92d4f
ivashkovaliza <ivashkovaliza@gmail.com>
2019-03-02 01:32:17 +0300

    Fix validation of Username, edit focus behavior on email input

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-var teamMemmbersArray = [];
+var teamMembersArray = [];
 var errorsAmount = 0;
 var errorsArray = [];
 var $regionnotice = $('<div class="regionnotice help is-danger" tabindex="-1">'),
@@ -12,32 +12,31 @@ $phoneError = $('<p id="phone-error" class="elementnotice help is-danger">Please
 $emailError = $('<p id="email-error" class="elementnotice help is-danger">Please enter a email (for example, name@address.com)</p>'),
 $yearError = $('<p id="year-error" class="elementnotice help is-danger">Please enter your year of birth (for example, 1999)</p>'),
 $emailValidationNotice = $('<p id="email-validation-notice">Email address validation started, please wait</p>'),
-$emailValidationSuccess = $('<p id="email-validation-notice">Validation successful</p>'),
-$emailValidationFail = $('<p id="email-validation-notice">Validation failed</p>');
+$emailValidationSuccess = $('<p id="email-validation-success">Validation successful</p>'),
+$emailValidationFail = $('<p id="email-validation-fail">Validation failed</p>');
+var isValidated = false; 
 
 document.querySelector('#submit-btn').addEventListener('click', addDataFromForm);
-document.querySelector('#email').addEventListener('focusout', checkEmailUniqueness);
+//document.querySelector('#email').addEventListener('focusout', checkEmailUniqueness);
+//document.querySelector('#email').addEventListener('focusin', willValidatedOnFocusout);
 
 function addDataFromForm(e) {
     e.preventDefault();
     resetFormState();
     checkValidityUsername(); 
-    checkValidityName();
-    checkValiditySurname();
-    checkValidityAddress();
-    checkValidityPhone();
-    checkValidityEmail();
-    if(teamMemmbersArray.length !== 0) {
-        console.log('unique email');
-        checkEqualNameAndSurname();
-    }
+    // checkValidityName();
+    // checkValiditySurname();
+    // checkValidityAddress();
+    // checkValidityPhone();
+    // checkValidityEmail();
+
+    console.log(errorsAmount);
 
     if(!document.getElementById('year-of-birth').hasAttribute('disabled')) {
         checkValidityYear();
     }
 
     if(errorsAmount) {
-        teamMemmbersArray.pop();
         $regionnotice.append($errorHeading);
         $regionnotice.append($listOfErrors);
         errorsArray.forEach( element => {
@@ -48,7 +47,7 @@ function addDataFromForm(e) {
         return;
     }     
 
-    teamMemmbersArray.push({
+    teamMembersArray.push({
         username: document.getElementById('username').value,
         name: document.getElementById('name').value,
         surname: document.getElementById('surname').value,
@@ -62,31 +61,34 @@ function addDataFromForm(e) {
     alert('The form is successfully submitted');
 }
 
-function checkEmailUniqueness(e) {
-    var that = this;
-    console.log('focusout email');
-    $('#email-field').append($emailValidationNotice);
-    $('#email').attr('aria-describedby', 'email-validation-notice');
-
-  $(that).focus();
-
-    setTimeout(() => {
-      teamMemmbersArray.forEach(element => {
-        if(element.email === document.getElementById('email').value) {
-            console.log('yes');
-          $emailValidationNotice.remove();
-          $('#email-field').append($emailValidationFail);
-          $(that).focusout();
-        } else {
-          $emailValidationNotice.remove();
-          $('#email-field').append($emailValidationSuccess);
-          console.log('no');
-          $(that).focusout();
-        }
-      });
-
-    }, 3000)
+function willValidatedOnFocusout() {
+    isValidated = true;
 }
+
+// function checkEmailUniqueness(e) {
+//     if(teamMembersArray.length !== 0 && isValidated) {
+
+//         var that = this;
+//         $('#email-field').append($emailValidationNotice);
+//         $('#email').attr('aria-describedby', 'email-validation-notice');
+
+
+//         $(that).focus();
+
+//         setTimeout(() => {
+//             teamMembersArray.forEach(element => {
+//                 if(element.email === document.getElementById('email').value) {
+//                     $emailValidationNotice.remove();
+//                     $('#email-field').append($emailValidationFail);
+//                 } else {
+//                     $emailValidationNotice.remove();
+//                     $('#email-field').append($emailValidationSuccess);
+//                 }
+//             });
+//             isValidated = false;
+//         }, 3000)
+//     }
+// }
 
 function checkValidityUsername() {
     $usernameError.remove();
@@ -97,8 +99,9 @@ function checkValidityUsername() {
         document.getElementById('username').setAttribute('aria-invalid','false');
         document.getElementById('username').removeAttribute('aria-describedby');
     }
-
-    teamMemmbersArray.forEach(element => {
+    console.log(teamMembersArray);
+    console.log(teamMembersArray.length);
+    teamMembersArray.forEach(element => {
         if(element.username === document.getElementById('username').value) {
             createUsernameError();            
         } else {
@@ -108,108 +111,108 @@ function checkValidityUsername() {
     });    
 }
 
-function checkValidityEmail() {
-    $emailError.remove();
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// function checkValidityEmail() {
+//     $emailError.remove();
+//     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
-    if(!re.test(document.getElementById('email').value)) {
-        errorsAmount++;
-        document.getElementById('email').setAttribute('aria-describedby','email-error');
-        document.getElementById('email').setAttribute('aria-invalid','true');
-        $('#email-field').append($emailError);
-        errorsArray.push($('<li><a href="#email">Email - Please enter a email (for example, name@address.com)</a></li>'));
-    }   else {
-        document.getElementById('email').setAttribute('aria-invalid','false');
-        document.getElementById('email').removeAttribute('aria-describedby');
-    }  
-}
+//     if(!re.test(document.getElementById('email').value)) {
+//         errorsAmount++;
+//         document.getElementById('email').setAttribute('aria-describedby','email-error');
+//         document.getElementById('email').setAttribute('aria-invalid','true');
+//         $('#email-field').append($emailError);
+//         errorsArray.push($('<li><a href="#email">Email - Please enter a email (for example, name@address.com)</a></li>'));
+//     }   else {
+//         document.getElementById('email').setAttribute('aria-invalid','false');
+//         document.getElementById('email').removeAttribute('aria-describedby');
+//     }  
+// }
 
-function checkValidityPhone() {
-    $phoneError.remove();
+// function checkValidityPhone() {
+//     $phoneError.remove();
 
-    if(!/^[0-9]+$/.test(document.getElementById('phone-number').value) || document.getElementById('phone-number').value === "") {
-        errorsAmount++;
-        document.getElementById('phone-number').setAttribute('aria-describedby','username-error');
-        document.getElementById('phone-number').setAttribute('aria-invalid','true');
-        $('#phone-field').append($phoneError);
-        errorsArray.push($('<li><a href="#phone-number">Phone number - Please enter a phone number (for example, 123456789)</a></li>'));
-    }   else {
-        document.getElementById('phone-number').setAttribute('aria-invalid','false');
-        document.getElementById('phone-number').removeAttribute('aria-describedby');
-    }     
-}
+//     if(!/^[0-9]+$/.test(document.getElementById('phone-number').value) || document.getElementById('phone-number').value === "") {
+//         errorsAmount++;
+//         document.getElementById('phone-number').setAttribute('aria-describedby','username-error');
+//         document.getElementById('phone-number').setAttribute('aria-invalid','true');
+//         $('#phone-field').append($phoneError);
+//         errorsArray.push($('<li><a href="#phone-number">Phone number - Please enter a phone number (for example, 123456789)</a></li>'));
+//     }   else {
+//         document.getElementById('phone-number').setAttribute('aria-invalid','false');
+//         document.getElementById('phone-number').removeAttribute('aria-describedby');
+//     }     
+// }
 
-function checkEqualNameAndSurname() {
-    $yearError.remove();
+// function checkEqualNameAndSurname() {
+//     $yearError.remove();
     
-    teamMemmbersArray.forEach(element => {
-        if(element.name === document.getElementById('name').value && element.surname === document.getElementById('surname').value) {
-            errorsAmount++;
-            document.getElementById('year-of-birth').removeAttribute('disabled');
-            document.getElementById('year-of-birth').setAttribute('aria-disabled','false');
-        }
-    });
-}
+//     teamMembersArray.forEach(element => {
+//         if(element.name === document.getElementById('name').value && element.surname === document.getElementById('surname').value) {
+//             errorsAmount++;
+//             document.getElementById('year-of-birth').removeAttribute('disabled');
+//             document.getElementById('year-of-birth').setAttribute('aria-disabled','false');
+//         }
+//     });
+// }
 
-function checkValidityName() {
-    $nameError.remove();
+// function checkValidityName() {
+//     $nameError.remove();
     
-    if(document.getElementById('name').value === "") {
-        errorsAmount++;
-        document.getElementById('name').setAttribute('aria-describedby','username-error');
-        document.getElementById('name').setAttribute('aria-invalid','true');
-        $('#name-field').append($nameError);
-        errorsArray.push($('<li><a href="#name">Name - Please enter a name</a></li>'));
-    } else {
-        document.getElementById('name').setAttribute('aria-invalid','false');
-        document.getElementById('name').removeAttribute('aria-describedby');
-    }       
-}
+//     if(document.getElementById('name').value === "") {
+//         errorsAmount++;
+//         document.getElementById('name').setAttribute('aria-describedby','username-error');
+//         document.getElementById('name').setAttribute('aria-invalid','true');
+//         $('#name-field').append($nameError);
+//         errorsArray.push($('<li><a href="#name">Name - Please enter a name</a></li>'));
+//     } else {
+//         document.getElementById('name').setAttribute('aria-invalid','false');
+//         document.getElementById('name').removeAttribute('aria-describedby');
+//     }       
+// }
 
-function checkValiditySurname() {
-    $surnameError.remove();
+// function checkValiditySurname() {
+//     $surnameError.remove();
     
-    if(document.getElementById('surname').value === "") {
-        errorsAmount++;
-        document.getElementById('surname').setAttribute('aria-describedby','surname-error');
-        document.getElementById('surname').setAttribute('aria-invalid','true');
-        $('#surname-field').append($surnameError);
-        errorsArray.push($('<li><a href="#surname">Surname - Please enter a surname</a></li>'));
-    } else {
-        document.getElementById('surname').setAttribute('aria-invalid','false');
-        document.getElementById('surname').removeAttribute('aria-describedby');
-    }       
-}
+//     if(document.getElementById('surname').value === "") {
+//         errorsAmount++;
+//         document.getElementById('surname').setAttribute('aria-describedby','surname-error');
+//         document.getElementById('surname').setAttribute('aria-invalid','true');
+//         $('#surname-field').append($surnameError);
+//         errorsArray.push($('<li><a href="#surname">Surname - Please enter a surname</a></li>'));
+//     } else {
+//         document.getElementById('surname').setAttribute('aria-invalid','false');
+//         document.getElementById('surname').removeAttribute('aria-describedby');
+//     }       
+// }
 
-function checkValidityAddress() {
-    $addressError.remove();
+// function checkValidityAddress() {
+//     $addressError.remove();
     
-    if(document.getElementById('address').value === "") {
-        errorsAmount++;
-        document.getElementById('address').setAttribute('aria-describedby','address-error');
-        document.getElementById('address').setAttribute('aria-invalid','true');
-        $('#address-field').append($addressError);
-        errorsArray.push($('<li><a href="#address">Address - Please enter an address</a></li>'));
-    } else {
-        document.getElementById('address').setAttribute('aria-invalid','false');
-        document.getElementById('address').removeAttribute('aria-describedby');
-    }       
-}
+//     if(document.getElementById('address').value === "") {
+//         errorsAmount++;
+//         document.getElementById('address').setAttribute('aria-describedby','address-error');
+//         document.getElementById('address').setAttribute('aria-invalid','true');
+//         $('#address-field').append($addressError);
+//         errorsArray.push($('<li><a href="#address">Address - Please enter an address</a></li>'));
+//     } else {
+//         document.getElementById('address').setAttribute('aria-invalid','false');
+//         document.getElementById('address').removeAttribute('aria-describedby');
+//     }       
+// }
 
-function checkValidityYear() {
-    $yearError.remove();
+// function checkValidityYear() {
+//     $yearError.remove();
     
-    if(!/^[1-9][0-9]{3}$/.test(document.getElementById('year-of-birth').value) || document.getElementById('year-of-birth').value === "") {
-        errorsAmount++;
-        document.getElementById('year-of-birth').setAttribute('aria-invalid','true');
-        document.getElementById('year-of-birth').setAttribute('aria-describedby','year-error');
-        $('#year-of-birth-field').append($yearError);
-        errorsArray.push($('<li><a href="#year-of-birth">Year of birth - Please enter your year of birth (for example, 1999)</a></li>'));
-    } else {
-        document.getElementById('year-of-birth').setAttribute('aria-invalid','false');
-        document.getElementById('year-of-birth').removeAttribute('aria-describedby');
-    } 
-}
+//     if(!/^[1-9][0-9]{3}$/.test(document.getElementById('year-of-birth').value) || document.getElementById('year-of-birth').value === "") {
+//         errorsAmount++;
+//         document.getElementById('year-of-birth').setAttribute('aria-invalid','true');
+//         document.getElementById('year-of-birth').setAttribute('aria-describedby','year-error');
+//         $('#year-of-birth-field').append($yearError);
+//         errorsArray.push($('<li><a href="#year-of-birth">Year of birth - Please enter your year of birth (for example, 1999)</a></li>'));
+//     } else {
+//         document.getElementById('year-of-birth').setAttribute('aria-invalid','false');
+//         document.getElementById('year-of-birth').removeAttribute('aria-describedby');
+//     } 
+// }
 
 function createUsernameError() {
     errorsAmount++;
