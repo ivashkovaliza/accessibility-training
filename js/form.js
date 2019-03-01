41eda1720be12972e4e961e9f184efe68de9f308
Elizaveta Ivashkova <Elizaveta_Ivashkova@epam.com>
2019-03-01 17:10:55 +0300

    Create message about validation start, create meassages for validation results

--- js/form.js
+++ js/form.js
@@ -1,18 +1,22 @@
-
-document.querySelector('#submit-btn').addEventListener('click', addDataFromForm);
 var teamMemmbersArray = [];
 var errorsAmount = 0;
 var errorsArray = [];
 var $regionnotice = $('<div class="regionnotice help is-danger" tabindex="-1">'),
 $errorHeading = $('<h3>Error! The form could not be submitted due to invalid entries.</h3><p>Please fix the following fields:</p>'),
 $listOfErrors = $('<ol>'),
-$usernameError = $('<p id="username-error" class="elementnotice help is-danger">This username is unavailable, please enter new username</p>');
-$nameError = $('<p id="name-error" class="elementnotice help is-danger">Please enter your name</p>');
-$surnameError = $('<p id="surname-error" class="elementnotice help is-danger">Please enter your surname</p>');
-$addressError = $('<p id="address-error" class="elementnotice help is-danger">Please enter your address</p>');
-$phoneError = $('<p id="phone-error" class="elementnotice help is-danger">Please enter your phone</p>');
-$emailError = $('<p id="email-error" class="elementnotice help is-danger">Please enter your email</p>');
-$yearError = $('<p id="year-error" class="elementnotice help is-danger">Please enter your year of birth</p>');
+$usernameError = $('<p id="username-error" class="elementnotice help is-danger">Please enter a unique username</p>'),
+$nameError = $('<p id="name-error" class="elementnotice help is-danger">Please enter your name</p>'),
+$surnameError = $('<p id="surname-error" class="elementnotice help is-danger">Please enter your surname</p>'),
+$addressError = $('<p id="address-error" class="elementnotice help is-danger">Please enter your address</p>'),
+$phoneError = $('<p id="phone-error" class="elementnotice help is-danger">Please enter a phone number (for example, 123456789)</p>'),
+$emailError = $('<p id="email-error" class="elementnotice help is-danger">Please enter a email (for example, name@address.com)</p>'),
+$yearError = $('<p id="year-error" class="elementnotice help is-danger">Please enter your year of birth (for example, 1999)</p>'),
+$emailValidationNotice = $('<p id="email-validation-notice">Email address validation started, please wait</p>'),
+$emailValidationSuccess = $('<p id="email-validation-notice">Validation successful</p>'),
+$emailValidationFail = $('<p id="email-validation-notice">Validation failed</p>');
+
+document.querySelector('#submit-btn').addEventListener('click', addDataFromForm);
+document.querySelector('#email').addEventListener('focusout', checkEmailUniqueness);
 
 function addDataFromForm(e) {
     e.preventDefault();
@@ -23,10 +27,12 @@ function addDataFromForm(e) {
     checkValidityAddress();
     checkValidityPhone();
     checkValidityEmail();
-    checkEqualNameAndSurname();
+    if(teamMemmbersArray.length !== 0) {
+        console.log('unique email');
+        checkEqualNameAndSurname();
+    }
 
     if(!document.getElementById('year-of-birth').hasAttribute('disabled')) {
-        console.log('Year error');
         checkValidityYear();
     }
 
@@ -52,16 +58,40 @@ function addDataFromForm(e) {
         postcode: document.getElementById('postcode').value,
         email: document.getElementById('email').value
     });
-  
-    console.log(teamMemmbersArray);
+
     alert('The form is successfully submitted');
+}
+
+function checkEmailUniqueness(e) {
+    var that = this;
+    console.log('focusout email');
+    $('#email-field').append($emailValidationNotice);
+    $('#email').attr('aria-describedby', 'email-validation-notice');
+
+  $(that).focus();
+
+    setTimeout(() => {
+      teamMemmbersArray.forEach(element => {
+        if(element.email === document.getElementById('email').value) {
+            console.log('yes');
+          $emailValidationNotice.remove();
+          $('#email-field').append($emailValidationFail);
+          $(that).focusout();
+        } else {
+          $emailValidationNotice.remove();
+          $('#email-field').append($emailValidationSuccess);
+          console.log('no');
+          $(that).focusout();
+        }
+      });
+
+    }, 3000)
 }
 
 function checkValidityUsername() {
     $usernameError.remove();
 
     if(document.getElementById('username').value === "") {
-        console.log('username-empty');
         createUsernameError()
     } else {
         document.getElementById('username').setAttribute('aria-invalid','false');
@@ -87,7 +117,7 @@ function checkValidityEmail() {
         document.getElementById('email').setAttribute('aria-describedby','email-error');
         document.getElementById('email').setAttribute('aria-invalid','true');
         $('#email-field').append($emailError);
-        errorsArray.push($('<li><a href="#email">Please enter a email</a></li>'));
+        errorsArray.push($('<li><a href="#email">Email - Please enter a email (for example, name@address.com)</a></li>'));
     }   else {
         document.getElementById('email').setAttribute('aria-invalid','false');
         document.getElementById('email').removeAttribute('aria-describedby');
@@ -102,7 +132,7 @@ function checkValidityPhone() {
         document.getElementById('phone-number').setAttribute('aria-describedby','username-error');
         document.getElementById('phone-number').setAttribute('aria-invalid','true');
         $('#phone-field').append($phoneError);
-        errorsArray.push($('<li><a href="#phone-number">Please enter a phone</a></li>'));
+        errorsArray.push($('<li><a href="#phone-number">Phone number - Please enter a phone number (for example, 123456789)</a></li>'));
     }   else {
         document.getElementById('phone-number').setAttribute('aria-invalid','false');
         document.getElementById('phone-number').removeAttribute('aria-describedby');
@@ -129,7 +159,7 @@ function checkValidityName() {
         document.getElementById('name').setAttribute('aria-describedby','username-error');
         document.getElementById('name').setAttribute('aria-invalid','true');
         $('#name-field').append($nameError);
-        errorsArray.push($('<li><a href="#name">Please enter a name</a></li>'));
+        errorsArray.push($('<li><a href="#name">Name - Please enter a name</a></li>'));
     } else {
         document.getElementById('name').setAttribute('aria-invalid','false');
         document.getElementById('name').removeAttribute('aria-describedby');
@@ -144,7 +174,7 @@ function checkValiditySurname() {
         document.getElementById('surname').setAttribute('aria-describedby','surname-error');
         document.getElementById('surname').setAttribute('aria-invalid','true');
         $('#surname-field').append($surnameError);
-        errorsArray.push($('<li><a href="#surname">Please enter a surname</a></li>'));
+        errorsArray.push($('<li><a href="#surname">Surname - Please enter a surname</a></li>'));
     } else {
         document.getElementById('surname').setAttribute('aria-invalid','false');
         document.getElementById('surname').removeAttribute('aria-describedby');
@@ -159,7 +189,7 @@ function checkValidityAddress() {
         document.getElementById('address').setAttribute('aria-describedby','address-error');
         document.getElementById('address').setAttribute('aria-invalid','true');
         $('#address-field').append($addressError);
-        errorsArray.push($('<li><a href="#address">Please enter an address</a></li>'));
+        errorsArray.push($('<li><a href="#address">Address - Please enter an address</a></li>'));
     } else {
         document.getElementById('address').setAttribute('aria-invalid','false');
         document.getElementById('address').removeAttribute('aria-describedby');
@@ -170,12 +200,11 @@ function checkValidityYear() {
     $yearError.remove();
     
     if(!/^[1-9][0-9]{3}$/.test(document.getElementById('year-of-birth').value) || document.getElementById('year-of-birth').value === "") {
-        console.log('checkValidityYear invalid')
         errorsAmount++;
         document.getElementById('year-of-birth').setAttribute('aria-invalid','true');
         document.getElementById('year-of-birth').setAttribute('aria-describedby','year-error');
         $('#year-of-birth-field').append($yearError);
-        errorsArray.push($('<li><a href="#year-of-birth">Please enter your year of birth</a></li>'));
+        errorsArray.push($('<li><a href="#year-of-birth">Year of birth - Please enter your year of birth (for example, 1999)</a></li>'));
     } else {
         document.getElementById('year-of-birth').setAttribute('aria-invalid','false');
         document.getElementById('year-of-birth').removeAttribute('aria-describedby');
@@ -187,7 +216,7 @@ function createUsernameError() {
     document.getElementById('username').setAttribute('aria-describedby','username-error');
     document.getElementById('username').setAttribute('aria-invalid','true');
     $('#username-field').append($usernameError);
-    errorsArray.push($('<li><a href="#username">Please enter a unique username</a></li>'));
+    errorsArray.push($('<li><a href="#username">Username - Please enter a unique username</a></li>'));
 }
 
 function resetFormState() {
@@ -196,3 +225,5 @@ function resetFormState() {
     errorsAmount = 0;
     errorsArray = [];    
 }
+
+
